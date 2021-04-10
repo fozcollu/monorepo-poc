08920eef238d56528a4c61730b8939cbe095eaf2
Ferhat özçöllü <ferhat.ozcollu@hesapkurdu.com>
2021-04-10 12:02:40 +0300

    .

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Text } from 'react-native';
 
 const App: React.FC = () => {
-  return <Text>Merhaba Dünya Mobile-Web</Text>;
+  return <Text>Merhaba Dünya Mobile-Web </Text>;
 };
 
 export default App;
